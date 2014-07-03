2298b3bc743e577b9a05d6a94b5207a7e1f15abe
alpeters78 <alpeters78@gmail.com>
2014-07-03 21:03:06 +0100

    xml grid for game board, working on nesting layouts to make it pretty

--- app/src/main/java/com/CMIS495/alpeters78/quoridor_android_app/Game.java
+++ app/src/main/java/com/CMIS495/alpeters78/quoridor_android_app/Game.java
@@ -2,19 +2,58 @@ package com.CMIS495.alpeters78.quoridor_android_app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.widget.ImageView;
 
 
 
 public class Game extends Activity {
 
+    private static final String TAG = Game.class.getSimpleName();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        /* I have been playing around with changing the image when clicked. I was thinking we can
+        only allow clicks on valid moves and then turning the image off when a new square is clicked.
+        Then parsing the ImageView's name to get the point the pawn was moved to.
+
+        ImageView pawn = (ImageView) findViewById(R.id.pawn11);
+
+        pawn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //This is how to change an image onClick
+                //ImageView imageView = (ImageView) findViewById(R.id.myimageview);
+                //imageView.setImageResource(R.drawable.myimage);
+
+                int position = view.getId();
+                findViewById(position);
+                String name = getResources().getResourceEntryName(position);
+                String clickPosition = name.substring(name.length(), -2);
+                int pawnPosition = Integer.parseInt(clickPosition);
+                int x = pawnPosition / 10;
+                int y = pawnPosition % 10;
+
+
+            }
+        });
+        */
+
     }
+
+
+
+
 
 
     @Override
@@ -35,6 +74,17 @@ public class Game extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    //Log events so we can see what is going on.
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "Destroying...");
+        super.onDestroy();
+    }
 
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopping...");
+        super.onStop();
+    }
 
 }
